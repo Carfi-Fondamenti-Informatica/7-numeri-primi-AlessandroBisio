944c12a10bebd349980637118d68b46c60e4d866
AlessandroBisio <91663842+AlessandroBisio@users.noreply.github.com>
2021-12-01 17:51:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+
+int main (){
+    int numero;
+    cin >> numero;
+    if (numero == 0){
+        cout <<" numero primo"<< endl;
+    }else if( numero < 0) {
+        return 0;
+    }else{
+
+    if (numeroPrimo(numero)== true){
+        cout << "numero primo" << endl;
+    }else{
+        cout << "numero non primo" << endl;
+    }
+        }
+    return 0;
 }
